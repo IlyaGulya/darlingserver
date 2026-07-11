9d5e7b6c29cdf74f43c73b41538a0dd86066e9ea
Ilya Gulya <ilya@gulya.me>
2026-07-11 21:36:49 +0000

    test: trace semaphore RPC lifecycle

--- internal-include/darlingserver/test-diagnostics.hpp
+++ internal-include/darlingserver/test-diagnostics.hpp
@@ -7,5 +7,24 @@ namespace DarlingServer::TestDiagnostics {
 bool enabled();
 bool consumeFault(const char* name);
 void traceLine(const std::string& line);
+void traceSemaphoreCallBegin(
+	const char* operation,
+	int pid,
+	int tid,
+	unsigned int waitName,
+	bool hasSignalName,
+	unsigned int signalName,
+	unsigned int sec,
+	unsigned int nsec
+);
+void traceSemaphoreCallReply(
+	const char* operation,
+	int pid,
+	int tid,
+	unsigned int waitName,
+	bool hasSignalName,
+	unsigned int signalName,
+	int resultCode
+);
 
 }
